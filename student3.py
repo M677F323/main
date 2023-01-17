8f4ecc2abd630fd82e646da30382322c9e81cc65
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-17 15:30:00 -0600

    updated

---
+++ student3.py
@@ -0,0 +1,13 @@
+import csv
+name =input("whats your name")
+home = input("whats your age")
+
+with open("students2.csv","a") as file:
+    writer = csv.writer(file)
+    #writer function take file argument
+    writer.writerow([name,home])
+
+with open("students2.csv","a") as file:
+    writer = csv.DictWriter(file,fieldname =["name","home"])
+    #writer function take file argument
+    writer.writerow({"name": name,"home":home})
