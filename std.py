d4f7b2509699ed566789b386b1e0e41f232d35b4
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-17 15:30:00 -0600

    Commit message for January 17, 2023

--- std.py
+++ std.py
@@ -1,32 +1,26 @@
-import csv
+""" import csv
 
 students = []
 
 # Configure the csv reader to handle multiple commas and newlines within a field
 with open("students.csv") as file:
-    reader = csv.reader(file, delimiter=',', quotechar='"')
-    for row in reader:
-        if len(row) >= 2:
-            name, home = row[:2]  # Only consider the first two values (name and home)
-            students.append({"name": name, "home": home})
-
+    reader = csv.reader(file)
+    for name,home in reader:
+            name, home =  students.append({"name": name, "home": home})
 # Sort and print the students
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['home']}")
+    print(f"{student['name']} is in {student['home']}") """
 
 
-
-#as dict reader
+import csv
 students = []
 
 # Configure the csv reader to handle multiple commas and newlines within a field
-with open("students.csv") as file:
-    reader = csv.DictReaderreader(file, delimiter=',', quotechar='"')
+with open("students2.csv") as file:
+    #returnd dictionaries
+    reader = csv.DictReader(file)
     for row in reader:
-        if len(row) >= 2:
-            name, home = row[:2]  # Only consider the first two values (name and home)
-            students.append({"name": name, "home": home})
-
+        students.append({"name": row["name"], "home": row["home"]})
 # Sort and print the students
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['home']}")
