4f5e177a37cd8663977f9ac98f3ad86a0a5f251a
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-26 04:22:34 -0600

    updated

--- packing.py
+++ packing.py
@@ -12,6 +12,7 @@ print(total(coins[0],coins[1],coins[2]))  """
 def total(galleons,sickels,knuts):
     return(galleons*17+sickels)*29+knuts
 
-coins=[100,50,25]
-print(total(*coins))
-#unpacking **************
+coins = {"galleons":100,"sickels":50,"knuts":25}
+#print(total(coins["galleons"],coins["sickles"],coins["knuts"]))
+print(total(**coins))
+
