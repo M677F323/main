68640e22df9f731fd618f7bca0c2b3b17fb0fcbb
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-27 06:15:34 -0600

    updated

--- yell.py
+++ yell.py
@@ -5,10 +5,21 @@ def yell(phrases):
         print(phrases.upper())
 main() """
 
-def main():
+""" def main():
     yell("this is heloo world")
 
 def yell(phrases):
         for phrase in phrases:
             print(phrase.upper(),end ="")
+main() """
+
+
+def main():
+    yell("this", "is", "heloo", "world")
+
+def yell(*phrases):
+     upper_case=[]
+     for phrase in phrases:
+        upper_case.append(phrase.upper())
+     print(*upper_case)
 main()
