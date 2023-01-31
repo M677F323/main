144ea549be126b0fb83a0156a167e0b4a2647063
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-31 10:30:20 -0600

    updated

--- usingmap1.py
+++ usingmap1.py
@@ -3,8 +3,15 @@ student["name"] for student in students if stduent["house"]= gryffyindor
 ]
 for gryffindor in sorted(gryffindors):
     print(gryffindor) """
-def gryffindor:
+""" def gryffindor:
     if s["house"] = gryffindor:
         return True
     else:
-        return False
+        return False """
+
+def _gryffindor:
+    return s["house"] = gryffindor #simply we can write like this
+gryffindors = filter(_gryffindor, students)
+for gryffindor in sorted(gryffindors key=lambda s:s["name"]):
+    print(gryffindor("name"))
+
