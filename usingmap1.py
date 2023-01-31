9a0dff43be446c185c925f7877641a96478283c9
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-01-31 10:30:20 -0600

    updated

---
+++ usingmap1.py
@@ -0,0 +1,10 @@
+""" gryffindors = [
+student["name"] for student in students if stduent["house"]= gryffyindor
+]
+for gryffindor in sorted(gryffindors):
+    print(gryffindor) """
+def gryffindor:
+    if s["house"] = gryffindor:
+        return True
+    else:
+        return False
