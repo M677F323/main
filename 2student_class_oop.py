72a0c8921e5e3eae792c44bb5f7403927d8b8795
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-03 12:15:03 -0500

    Updating classes

--- 2student_class_oop.py
+++ 2student_class_oop.py
@@ -1,4 +1,4 @@
-class Student:
+""" class Student:
     ...
     #this mean we can implement later
 
@@ -14,3 +14,23 @@ def get_student():
 # __ this is dunder
 if __name__ == "__main__":
     main()
+     """
+
+
+
+class Student:
+    ...
+    #this mean we can implement later
+    def __init__(self,name,house):
+        self.name = name
+        self.house = house
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+def get_student():
+    name = input("Name :")
+    house = input("House: ")
+    student = Student(name,house)
+    return student
+if __name__ == "__main__":
+    main()    
