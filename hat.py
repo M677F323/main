db7ed13b57dbe9e90d0f47103b2936ec3d61ab42
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-07 16:28:22 -0500

    Updating classes

--- hat.py
+++ hat.py
@@ -8,3 +8,11 @@ class Hat:
 
 hat = Hat()
 hat.sort("harry")
+
+
+""" #even normally we cn write but when code become complicated then we cannt maage it
+import random
+self.houses = ["guntur","vijayawada","Texas","Austin","europe"]
+def sort(name):
+    print(name ,"is in ",random.choice(houses))
+hat.sort("harry") """
