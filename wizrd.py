aa2a92888ff1e3bb07458aeee4267fb9944fa9d2
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-08 17:40:54 -0500

    Updating classes

--- wizrd.py
+++ wizrd.py
@@ -1,19 +1,19 @@
-class Wizard():
-def __init__(self,name):
+class Wizard:
+    def __init__(self,name):
         if not name:
             return ValueError("Missing Name")
         self.name = name
 #inheritance like (wizard)
 #super().__init__()
-class Student(wizard):
+class Student(Wizard):
     def __init__(self,name,house):
         #if not name:
         #  return ValueError("Missing Name")
         #self.name = name
         super().__init__(name)
         self.house = house
-class Professor(wizard):
-    def __init__(self,name,subject)
+class Professor(Wizard):
+    def __init__(self,name,subject):
         #if not name:
             #return ValueError("Missing Name")
         #self.name = name
@@ -24,4 +24,3 @@ wizard = Wizard("veera")
 student = Student("kin","china")
 professor = Professor("malen","cs50")
 
-    
