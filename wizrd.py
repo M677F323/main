f57262ce5d0b394ba218b000319126c30f7b552c
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-08 20:44:32 -0500

    Updating classes

---
+++ wizrd.py
@@ -0,0 +1,15 @@
+def wizrad():
+    ...
+def student(self,name,house):
+    if not name:
+        return ValueError("Missing Name")
+    self.name = name
+    self.house = house
+
+def professor(self,name,subject)
+    if not name:
+        return ValueError("Missing Name")
+    self.name = name
+    self.subject = subject
+
+    
