f4d56ff25aa2132aca8c6d2c76dd11d208657321
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-08 23:12:22 -0500

    Updating classes

--- wizrd.py
+++ wizrd.py
@@ -1,14 +1,19 @@
-
+def wizrad():
+    def __init__(self,name):
+        if not name:
+            return ValueError("Missing Name")
+        self.name = name
+class Student(wizard):
 def student(self,name,house):
-    if not name:
-        return ValueError("Missing Name")
-    self.name = name
+    #if not name:
+      #  return ValueError("Missing Name")
+    #self.name = name
     self.house = house
-
+class professor(wizard):
 def professor(self,name,subject)
-    if not name:
-        return ValueError("Missing Name")
-    self.name = name
+    #if not name:
+        #return ValueError("Missing Name")
+    #self.name = name
     self.subject = subject
 
     
