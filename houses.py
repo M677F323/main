fcc2c879da45781168207e7b00dcf8564997ada7
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-10 18:55:54 -0500

    Updating classes

---
+++ houses.py
@@ -0,0 +1,18 @@
+students =[
+    {"name":"veera","house":"guntur"},
+    {"name":"ven","house":"vij"},
+    {"name":"vira","house":"ban"},
+    {"name":"keshav","house":"guntur"},
+    {"name":"Kalyan","house":"guntur"}
+]
+""" houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+houses.sort()
+print(houses) """
+#it automatically sort and remove duplicates
+houses = set()
+for student in students:
+    houses.add(student["house"])
+print(houses)
