6b4e86da8bb519c8e4bd9189d1cdfe689576d42d
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-15 12:15:03 -0500

    updated regex

--- format.py
+++ format.py
@@ -4,9 +4,10 @@ name = input("whats your name? ").strip()
     last, first = name.split(",")
     name = f"{first}{last}"
 print(f"hello,{name}") """
-matches =re.search(r"^().+), (.+)$", name)
+matches =re.search(r"^(.+), (.+)$", name)
+#if matches := re.search(r"^(.+), ).+)$", name)
 if matches:
-    last ,first = matches.groups()
+    #last ,first = matches.groups()
     #name = f"{first} {last}"
     #first = matches.proup(1)
     #last = matches.group(2)
