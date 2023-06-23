54096979e3a1066c6064af9156c0661c38efed6c
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-06-23 10:30:20 -0500

    updated classes

--- meow.py
+++ meow.py
@@ -1,4 +1,14 @@
-MEOW = 3
+""" MEOW = 3
 MEOW = 4 
 for _ in range(MEOW):
     print("meow")
+ """
+
+class Cat:
+    MEOW =3
+    def meow(SELF):
+        for _ in range(Cat.MEOW):
+            print("meow")
+
+cat =  Cat()
+cat.meow()
