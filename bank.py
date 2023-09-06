e711cb8bf69ed497c374c23d0d04685229f0b07f
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-06 02:25:24 -0500

    bank global

--- bank.py
+++ bank.py
@@ -1,8 +1,26 @@
+""" balance = 0
+
+def main():
+    print("balance: ", balance)
+
+if __name__ == "__main__":
+    main() """
+
 balance = 0
 
 def main():
+    deposit(100)
+    withdraw(50)
+    print("balance :" ,balance)
 
-    print("balance: ", balance)
+def deposit(n):
+    global balance #This is for modifiying the global variable 
+    #without this global varibale in local we cannt update the balanace which is  outside
+    balance += n
+
+def withdraw(n):
+    global balance
+    balance -= n
 
 if __name__ == "__main__":
     main()
