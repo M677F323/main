85293c639363edf36bf8cad6bd514725014f6b2b
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-08 15:13:59 -0500

    new

--- parity.py
+++ parity.py
@@ -1,2 +1,23 @@
+'''
 x =int(input("whats the values of x"))
 
+if x%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+'''
+
+def main():
+    x =int(input("whats the values of x"))
+    if is_even(x):
+        print("even")
+    else:
+        print("odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
