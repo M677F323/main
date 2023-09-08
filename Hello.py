ce2dabab6c1e9a1f3838c8ce6d6bcd13a7cf8e6d
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-08 13:07:12 -0500

    upadted

--- Hello.py
+++ Hello.py
@@ -13,4 +13,27 @@ string in python is str
 sep='', default seperate by the single space
 end='\n' its default
 print(*objetcs, sep='', end='\n', file=sys.stdout, flush =False)
+we can over ride those like  end="" then it will not move to next line
 """
+name = name.strip() #eliminate the white spaces front and end we can also use lstrip() and rstrip()
+name = name.capitalize() #it like  Veera sekhar only first letter is capital
+name = name.title() #make every frist letter capital like Veera Sekhar
+
+#we can also combine all of them like below this
+name = name.strip().title()
+
+#and als0 we can also do like this
+name = input("whats you name? ").strip().title()
+
+#splitting and assigning the names
+first,last = name.split("")
+print('hello,"world"')
+
+#we can use back slashes so it think that it is sub literal ot sub string ( \"  \ " )
+print('hello,\"world\"')
+
+#another way to import or format name
+print(f"hello, {name}")
+
+
+
