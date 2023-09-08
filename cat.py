e29e9e9da364e8691f16834483e831814c2f4ea3
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-08 16:47:03 -0500

    updated

--- cat.py
+++ cat.py
@@ -45,3 +45,4 @@ def meow(n):
     for _ in range(n):
         print("meow")
 
+main()
---
+++ hogwarts.py
@@ -0,0 +1,14 @@
+students =["herm","veera","ion"]
+'''
+for student in students:
+    print(student)
+'''
+#range expects integer only but but we can use "len"
+
+for i in range(len(students)):
+    print(i, students[i])
+'''
+v=["veera","kim"]
+for i in range(len(v)):
+    print(i, v[i])
+'''
