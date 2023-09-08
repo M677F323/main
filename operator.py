dd4f3f92cf568bc7a7e7eaa375bea9eaf8119a09
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-08 14:59:20 -0500

    update

---
+++ operator.py
@@ -0,0 +1,21 @@
+x= int(input("what is x"))
+y = int(input("what is y"))
+
+if x<y or x>y:
+    print("x is not equall to y")
+else:
+    print("x is not equal to y")
+
+
+#we can simple write
+if (x!=y)
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+#or else we can writee
+
+if (x==y)
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
