a4fd7a4ace7676508d1f1861e347ac063603e6b9
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-08 14:48:13 -0500

    update

---
+++ compare.py
@@ -0,0 +1,11 @@
+x= int(input("what is x"))
+y = int(input("what is y"))
+
+if x < y:
+    print("x is less than y")
+elif x >y:
+    print("x is greter than y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("Not valid")
