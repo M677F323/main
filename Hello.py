2cf7ed2dbe10a25e18140fd472763c82f6ef066a
vslocal <vrskhr1999@gmail.com>
2023-09-08 10:13:39 -0500

    Hello.py

---
+++ Hello.py
@@ -0,0 +1,15 @@
+#print function is predefined to print a variable or string
+#argumets we are passing to the function
+print("hello world")
+
+# Take input form the user
+name = input("whats your name: ")
+
+#concatanating name to string
+print("Hello," + name) 
+
+#string in python is str
+#*objecst mean any no of strings we can take
+#sep='', default seperate by the single space
+#end='\n' its default
+print(*objetcs, sep='', end='\n', file=sys.stdout, flush =False)
