03be6a46c597fb8c87167444a922e934af61ec52
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-10 15:19:13 -0500

    api itunes data

---
+++ api.py
@@ -0,0 +1,19 @@
+#API Application programming interface connection b/w code and userinterface  
+#it use web browser and also download thirdparty sites
+
+#Requests it is most important library in the these API
+
+import requests
+import json
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("too few")
+#its like downloading and assigning it to respons add adding argument vector
+response =requests.get("https://itunes.apple.com/search?entity=song&limit=2&term="+sys.argv[1])
+#json() to give output as json
+print(response.json())
+#json.dumps to decorate
+o = (response.json())
+for arg in o["results"]:
+    print(arg["trackName"])
