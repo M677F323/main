1fccdd5119f1d5df488968abde9e981208bb8a6f
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 15:51:35 -0500

    Commit message for January 17, 2023

--- std.py
+++ std.py
@@ -13,3 +13,20 @@ with open("students.csv") as file:
 # Sort and print the students
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['home']}")
+
+
+
+#as dict reader
+students = []
+
+# Configure the csv reader to handle multiple commas and newlines within a field
+with open("students.csv") as file:
+    reader = csv.DictReaderreader(file, delimiter=',', quotechar='"')
+    for row in reader:
+        if len(row) >= 2:
+            name, home = row[:2]  # Only consider the first two values (name and home)
+            students.append({"name": name, "home": home})
+
+# Sort and print the students
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['home']}")
