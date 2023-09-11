1cd94fe4a07dbc8ccfc32648a11c1186f7068f21
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 09:57:12 -0500

    multiple tests

--- test_calc.py
+++ test_calc.py
@@ -12,9 +12,28 @@ if __name__ == "__main__":
     main()
  """
 # assert  mean make that true  like something is true  it raises errors  if it is false
+#to  catch assertioon error
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 square was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 square is not 9")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 square is not 9")   
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 square is not 4")  
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 square is not 0")  
 #here we nodded to use the  if condition
 if __name__ == "__main__":
     main()
