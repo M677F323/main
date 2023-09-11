5ee37166d7609ce8853e5c4a9f8a4ce7b410fa7d
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 14:55:25 -0500

    updated

--- students.py
+++ students.py
@@ -3,10 +3,10 @@
         row = line.rstrip().split(",")
         print(f"{row[0]} is in {row[1]}") """
 #students = []
-with open("students.csv") as file:
+""" with open("students.csv") as file:
     for line in sorted(file):
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        print(f"{name} is in {house}") """
         #students.append(f"{name} is in {house}")
 #for student in sorted(students):
 #    print(student)
@@ -17,13 +17,19 @@ with open("students.csv") as file:
      #sorted according to line
     #for line in sorted(file):
     for line in file:
-        name, house = line.rstrip().split(",")
+        name,house = line.rstrip().split(",")
         print(f"{name} is in {house}")
         #creating empty dictionary
-"""     student = {}
+        """student = {}
         student["name"] =name
-        student["house"]=house """
-        student = {"name":name, "house":house}
+        student["house"]=house"""
+
+        student = {"name": name, "house": house}
         students.append(student)
-for student in students:
-    print{f"{student['name']} is in {student['house']}"}
+#sort according to studnets name
+def get_name():
+    return students["name"]
+def get_house():
+    return students["house"]
+for student in sorted(students, key = get_name):
+    print(f"{student['name']} is in {student['house']}")
