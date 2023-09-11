c575dd695706aa9d277ca15e1b0b71aa4127137b
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 13:55:45 -0500

    updated

--- names.py
+++ names.py
@@ -27,5 +27,5 @@ names= []
 with open("names.txt") as file:
    for line in file:
         names.append(line.rstrip())
-for name in sorted(names)
+for name in sorted(names):
     print(f"hello,{name}")
