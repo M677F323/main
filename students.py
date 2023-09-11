522d106085007ed725c7fa66c81a59f69ab5ecd3
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 14:26:16 -0500

    updated

--- students.py
+++ students.py
@@ -1,4 +1,12 @@
-with open("students.csv") as file:
+""" with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        print(f"{row[0]} is in {row[1]}") """
+#students = []
+with open("students.csv") as file:
+    for line in sorted(file):
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+        #students.append(f"{name} is in {house}")
+#for student in sorted(students):
+#    print(student)
