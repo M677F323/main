c55698512950ebaaf817fa480b637092c3004eba
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 15:17:31 -0500

    updated with the lambda function

--- students.py
+++ students.py
@@ -21,9 +21,11 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 #sort according to studnets name
-def get_name(student):
+""" def get_name(student):
     return student["name"]
 def get_house(student):
     return student["house"]
-for student in sorted(students, key = get_name):
+for student in sorted(students, key = get_name): """
+#by using lambda ananymous function
+for student in sorted(students, key = lambda student :student["name"]):
     print(f"{student['name']} is in {student['house']}")
