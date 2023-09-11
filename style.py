7e4c5af383b37f4c00cb526c49377253072a27c2
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 08:57:33 -0500

    its about pep8

---
+++ style.py
@@ -0,0 +1,5 @@
+#PEP 8 PYTHON ENHANCEMENT PROPOSAL  MAKE OUR CODE LOOK GOOD
+#pepes.python .pep 8 org
+#"readabiity counts" it all about consitency 
+#indentattion and tabs,spaces and maximun line and blank lines and imports and furthermore
+
