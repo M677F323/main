a49c409737cad49e8eee5a2f079cd4c7fc9ad600
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-11 14:37:48 -0500

    seperating the keys to sort accordingly

--- students.py
+++ students.py
@@ -10,3 +10,20 @@ with open("students.csv") as file:
         #students.append(f"{name} is in {house}")
 #for student in sorted(students):
 #    print(student)
+
+
+students = []
+with open("students.csv") as file:
+     #sorted according to line
+    #for line in sorted(file):
+    for line in file:
+        name, house = line.rstrip().split(",")
+        print(f"{name} is in {house}")
+        #creating empty dictionary
+"""     student = {}
+        student["name"] =name
+        student["house"]=house """
+        student = {"name":name, "house":house}
+        students.append(student)
+for student in students:
+    print{f"{student['name']} is in {student['house']}"}
