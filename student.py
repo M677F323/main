cf60b9d14680916146d4485dce53dc11f5064fe6
VEERASEKHAR-GIT <m677f323@wichita.edu>
2023-09-13 08:06:03 -0500

    return with only one one get_function

--- student.py
+++ student.py
@@ -2,7 +2,7 @@
 house = input("house")
 print(f"{name} from {house}") """
 
-def main():
+""" def main():
     name = get_name()
     house = get_house()
     print(f"{name} from {house}")
@@ -10,5 +10,16 @@ def get_name():
     return input("Name:")
 def get_house():
     return input("House:")
+if __name__ == "__main__":
+    main() """
+
+def main():
+    name,house = get_student()
+
+    print(f"{name} from {house}")
+def get_student():
+    name = input("Name: ")
+    house = input("house: ")
+    return name,house
 if __name__ == "__main__":
     main()
